073fc3579d650a3cd12eb667a126e96a8a779b36
The Jim Factor <thejimfactor@gmail.com>
2013-08-15 17:48:05 +0000

    added Google plus

--- grabber/crawl1.py
+++ grabber/crawl1.py
@@ -8,11 +8,18 @@ doc = parse(url).getroot()
 
 # social site and its account name regular expression
 patterns = {
-	'Facebook':r'https://www.facebook.com/([a-zA-Z]*)"',
-	'Twitter':r'https://twitter.com/([a-zA-Z]*)"'
+	'Facebook':r'https://www.facebook.com/([a-zA-Z]+)"',
+	'Twitter':r'https://twitter.com/([a-zA-Z]+)"',
+	'Google Plus':r'https://plus.google.com/\+([a-zA-Z])+/"'
 	}
+
+# matchestest = re.search(patterns['Google Plus'], tostring(doc))
+# print "%s Account - %s" % ('Google Plus', matchestest)
+# print "%s Account - %s" % ('Google Plus', matchestest.group(1))
 
 for social_site in patterns:
 	matches = re.search(patterns[social_site], tostring(doc))
-	print "%s Account - %s" % (social_site, matches.group(1))
-
+	try:
+		print "%s Account - %s" % (social_site, matches.group(1))
+	except (AttributeError):
+		print "No %s Account Found" % (social_site)
